e6a24f8eb80773471dd9a0f5d60db89d36824892
João Ribeiro <a13832@alunos.ipca.pt>
2025-09-09 14:12:23 +0100

    Update SocialFloatingBubble.tsx

--- src/components/SocialFloatingBubble.tsx
+++ src/components/SocialFloatingBubble.tsx
@@ -23,7 +23,7 @@ const SocialFloatingBubble = () => {
 
   const openWhatsApp = () => {
     const message = encodeURIComponent(
-      "Olá Nelson, estou super animada/o para começar com este programa. Podemos avançar :)"
+      "Olá Nelson, estou super animada/o para começar com este programa. Podemos avançar!"
     );
     window.open(`https://wa.me/${whatsappNumber}?text=${message}`, "_blank");
   };
